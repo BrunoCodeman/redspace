fa2918b2b83bfb1efe174845b678687bfb2973ff
Bruno Bemfica <bruno.bemfica@udemy.com>
2021-07-17 22:57:40 -0300

    Adding declarations and routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/smart/home/home.component';
+import {MatButtonModule} from '@angular/material/button';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatIconModule} from '@angular/material/icon';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { PeopleComponent } from './components/smart/people/people.component';
+import { MoviesComponent } from './components/smart/movies/movies.component';
+import { PlanetsComponent } from './components/smart/planets/planets.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    PeopleComponent,
+    MoviesComponent,
+    PlanetsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    MatButtonModule,
+    MatIconModule,
+    MatDividerModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
