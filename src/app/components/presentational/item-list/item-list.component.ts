e889b9a2927a6b8e82924bd45196bee027471418
Bruno Bemfica <bruno.bemfica@udemy.com>
2021-07-18 02:07:24 -0300

    creating item list component to show query results

---
+++ src/app/components/presentational/item-list/item-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'item-list',
+  templateUrl: './item-list.component.html',
+  styleUrls: ['./item-list.component.scss']
+})
+export class ItemListComponent implements OnInit {
+  @Input() items = new Array<any>();
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
