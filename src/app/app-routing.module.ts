522f44725a265665998a463a7ba3b916dd4d10d0
Bruno Bemfica <bruno.bemfica@udemy.com>
2021-07-19 14:21:29 -0300

    adding other routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,12 @@ const routes: Routes = [
   loadChildren: () => import('./components/smart/people/people.module').then(m => m.PeopleModule) },
   { path: 'people', 
   loadChildren: () => import('./components/smart/people/people.module').then(m => m.PeopleModule) },
+
+  { path: 'movies', 
+  loadChildren: () => import('./components/smart/films/films.module').then(m => m.FilmsModule) },
+
+  { path: 'planets', 
+  loadChildren: () => import('./components/smart/planets/planets.module').then(m => m.PlanetsModule) },
 ];
 
 @NgModule({
