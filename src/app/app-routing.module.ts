a7c478bbecb64f5e0daa57fabb88d9dca5881f92
Bruno Bemfica <bruno.bemfica@udemy.com>
2021-07-19 14:09:18 -0300

    updating main module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { FilmDetailComponent } from './components/presentational/film-detail/film-detail.component';
-import { PersonDetailComponent } from './components/presentational/person-detail/person-detail.component';
-import { PlanetDetailComponent } from './components/presentational/planet-detail/planet-detail.component';
-import { FilmsComponent } from './components/smart/films/films.component';
-import { PeopleComponent } from './components/smart/people/people.component';
-import { PlanetsComponent } from './components/smart/planets/planets.component';
 
 const routes: Routes = [
-  {path: "", component: PeopleComponent},
-  {path:"people", component: PeopleComponent},
-  {path:"person", component: PersonDetailComponent},
-  {path:"movies", component: FilmsComponent},
-  {path:"film", component: FilmDetailComponent},
-  {path:"planets", component: PlanetsComponent},
-  {path:"planet", component: PlanetDetailComponent},
+  { path: '', 
+  loadChildren: () => import('./components/smart/people/people.module').then(m => m.PeopleModule) },
+  { path: 'people', 
+  loadChildren: () => import('./components/smart/people/people.module').then(m => m.PeopleModule) },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatIconModule } from '@angular/material/icon';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { PeopleComponent } from './components/smart/people/people.component';
-import { FilmsComponent } from './components/smart/films/films.component';
-import { PlanetsComponent } from './components/smart/planets/planets.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { BottomNavModule } from 'ngx-bottom-nav';
-import { ItemSearchComponent } from './components/presentational/item-search/item-search.component';
-import { ItemListComponent } from './components/presentational/item-list/item-list.component';
 import { MatListModule } from '@angular/material/list';
-import { ItemComponent } from './components/presentational/item/item.component';
 import { MatCardModule } from '@angular/material/card';
-import { PersonDetailComponent } from './components/presentational/person-detail/person-detail.component';
 import { FormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
@@ -27,41 +19,37 @@ import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { BaseService } from './services/base-service.service';
 import { PeopleService } from './services/people.service';
 import { MatSidenavModule } from '@angular/material/sidenav';
-import { PlanetDetailComponent } from './components/presentational/planet-detail/planet-detail.component';
-import { FilmDetailComponent } from './components/presentational/film-detail/film-detail.component';
+import { PlanetsService } from './services/planets.service';
+import { FilmsService } from './services/films.service';
+import { PeopleModule } from './components/smart/people/people.module';
+import { FilmsModule } from './components/smart/films/films.module';
+import { PlanetsModule } from './components/smart/planets/planets.module';
 @NgModule({
   declarations: [
     AppComponent,
-    PeopleComponent,
-    FilmsComponent,
-    PlanetsComponent,
-    ItemListComponent,
-    ItemSearchComponent,
-    ItemComponent,
-    PersonDetailComponent,
-    PlanetDetailComponent,
-    FilmDetailComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
     BrowserAnimationsModule,
     AppRoutingModule,
-    MatButtonModule,
-    MatIconModule,
-    MatDividerModule,
-    MatToolbarModule,
-    BottomNavModule,
-    MatListModule,
-    MatCardModule,
-    MatFormFieldModule,
-    MatInputModule,
+    // MatButtonModule,
+     MatIconModule,
+    // MatDividerModule,
+     MatToolbarModule,
+     BottomNavModule,
+    // MatListModule,
+    // MatCardModule,
+    // MatFormFieldModule,
+    // MatInputModule,
     HttpClientModule,
-    MatProgressSpinnerModule,
-    MatSidenavModule
-
+    // MatProgressSpinnerModule,
+    // MatSidenavModule,
+    PeopleModule,
+    PlanetsModule,
+    FilmsModule
   ],
-  providers: [HttpClient, BaseService, PeopleService],
+  providers: [HttpClient, BaseService, PlanetsService, PeopleService, FilmsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
