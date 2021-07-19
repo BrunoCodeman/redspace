0a3ca3cee9c8649fa33a30e34fbadb46bde29d0a
Bruno Bemfica <bruno.bemfica@udemy.com>
2021-07-19 00:55:28 -0300

    implementing film search

---
+++ src/app/components/smart/films/films.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { Item } from 'src/app/models/item';
+import { FilmsService } from 'src/app/services/films.service';
+
+@Component({
+  selector: 'app-films',
+  templateUrl: './films.component.html',
+  styleUrls: ['./films.component.scss']
+})
+export class FilmsComponent implements OnInit {
+
+  _films: BehaviorSubject<Array<Item>> = new BehaviorSubject(new Array());
+
+  constructor(public service: FilmsService) { }
+
+  ngOnInit(): void {
+    this.searchFilms("");
+   }
+
+  searchFilms(event: any) {
+    this.service.search(event).subscribe((data) => {
+      const items = new Array<Item>();
+      data.results.forEach(e => items.push({
+        icon: "movies", model:e,
+        route: "film", title: e.title
+      }));
+      this._films.next(items);
+    });    
+  }
+}
