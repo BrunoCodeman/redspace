68c3e6a3897ddd62b6af619e43d3d0199f01945d
Bruno Bemfica <bruno.bemfica@udemy.com>
2021-07-19 00:37:38 -0300

    finishing item search

--- src/app/components/presentational/item-search/item-search.component.ts
+++ src/app/components/presentational/item-search/item-search.component.ts
@@ -1,4 +1,6 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { Item } from 'src/app/models/item';
 
 @Component({
   selector: 'item-search',
@@ -6,16 +8,23 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./item-search.component.scss']
 })
 export class ItemSearchComponent implements OnInit {
-  @Input() url = "";
-  value = "";
-  items = [
-    {title:"Luke Skywalker", icon:"public", link:"/blabla/1"},
-    {title:"Princess Lea", icon:"public", link:"/blabla/1"},
-    {title:"Obi Wan Kenobi", icon:"public", link:"/blabla/1"},
-  ];
-  constructor() { }
+  @Output("searchTerm") searchTerm: EventEmitter<string> = new EventEmitter<string>();
+  @Input() searchResults = new BehaviorSubject<Array<Item>>([]);
 
-  ngOnInit(): void {
+  value = "";
+  constructor() {  }
+
+  ngOnInit(): void { 
+   }
+
+  typeSearch(event: KeyboardEvent) {
+    if (this.value.length > 2) {
+      this.searchTerm.emit(this.value);
+    }
   }
 
+  clearData($event: any) {
+    this.value = '';
+    this.searchResults.next([]);
+  }
 }
