87d790222d49399d5a04c74b7fd86bef48706955
Bruno Bemfica <bruno.bemfica@udemy.com>
2021-07-19 00:37:24 -0300

    finishing item list

--- src/app/components/presentational/item-list/item-list.component.ts
+++ src/app/components/presentational/item-list/item-list.component.ts
@@ -1,15 +1,24 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { Item } from 'src/app/models/item';
 
 @Component({
   selector: 'item-list',
   templateUrl: './item-list.component.html',
   styleUrls: ['./item-list.component.scss']
 })
-export class ItemListComponent implements OnInit {
-  @Input() items = new Array<any>();
+export class ItemListComponent implements OnInit, OnDestroy {
+  @Input() items: BehaviorSubject<Array<Item>> = new BehaviorSubject(new Array());
+  isLoadingData = true;
   constructor() { }
 
   ngOnInit(): void {
+    this.items.subscribe((data) => {
+      this.isLoadingData = false;
+    });
+  }
+  ngOnDestroy(){
+    this.items.unsubscribe();
   }
 
 }
