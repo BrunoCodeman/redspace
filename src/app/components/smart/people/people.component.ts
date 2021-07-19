9f6b9df5aaa533549416c6d7c9978d63e3186968
Bruno Bemfica <bruno.bemfica@udemy.com>
2021-07-19 00:38:26 -0300

    finishing people list component

--- src/app/components/smart/people/people.component.ts
+++ src/app/components/smart/people/people.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { Person } from 'src/app/models/person';
+import { Item } from 'src/app/models/item';
+import { PeopleService } from 'src/app/services/people.service';
 
 @Component({
   selector: 'app-people',
@@ -6,10 +10,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./people.component.scss']
 })
 export class PeopleComponent implements OnInit {
-  url = "";
-  constructor() { }
+  _people: BehaviorSubject<Array<Item>> = new BehaviorSubject(new Array());
+
+  constructor(public service: PeopleService) { }
 
   ngOnInit(): void {
-  }
+    this.searchPeople("");
+   }
 
+  searchPeople(event: any) {
+    this.service.search(event).subscribe((data) => {
+      const items = new Array<Item>();
+      data.results.forEach(e => items.push({
+        icon: "people_outline", model:e,
+        route: "people", title: e.name
+      }));
+      this._people.next(items);
+    });    
+  }
 }
