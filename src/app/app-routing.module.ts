3d63572b4df7515d935857b7713e50b519c81224
Bruno Bemfica <bruno.bemfica@udemy.com>
2021-07-19 02:00:48 -0300

    adding new routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PersonDetailComponent } from './components/presentational/person-detail/person-detail.component';
+import { PlanetDetailComponent } from './components/presentational/planet-detail/planet-detail.component';
 import { FilmsComponent } from './components/smart/films/films.component';
 import { PeopleComponent } from './components/smart/people/people.component';
 import { PlanetsComponent } from './components/smart/planets/planets.component';
@@ -11,6 +12,7 @@ const routes: Routes = [
   {path:"person", component: PersonDetailComponent},
   {path:"movies", component: FilmsComponent},
   {path:"planets", component: PlanetsComponent},
+  {path:"planet", component: PlanetDetailComponent},
 ];
 
 @NgModule({
