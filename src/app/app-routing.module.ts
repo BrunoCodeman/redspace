d46f07eb34854ff169cabc925e9dcebdfce08ec6
Bruno Bemfica <bruno.bemfica@udemy.com>
2021-07-19 01:00:33 -0300

    redirecting home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './components/smart/home/home.component';
 import { FilmsComponent } from './components/smart/films/films.component';
 import { PeopleComponent } from './components/smart/people/people.component';
 import { PlanetsComponent } from './components/smart/planets/planets.component';
 
 const routes: Routes = [
-  {path: "", component: HomeComponent},
+  {path: "", component: PeopleComponent},
   {path:"people", component: PeopleComponent},
   {path:"movies", component: FilmsComponent},
   {path:"planets", component: PlanetsComponent},
