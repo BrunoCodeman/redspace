9ed6942659bf4c22d3f4abe6636391b871fd8a59
Bruno Bemfica <bruno.bemfica@udemy.com>
2021-07-19 01:51:38 -0300

    adding person route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PersonDetailComponent } from './components/presentational/person-detail/person-detail.component';
 import { FilmsComponent } from './components/smart/films/films.component';
 import { PeopleComponent } from './components/smart/people/people.component';
 import { PlanetsComponent } from './components/smart/planets/planets.component';
@@ -7,6 +8,7 @@ import { PlanetsComponent } from './components/smart/planets/planets.component';
 const routes: Routes = [
   {path: "", component: PeopleComponent},
   {path:"people", component: PeopleComponent},
+  {path:"person", component: PersonDetailComponent},
   {path:"movies", component: FilmsComponent},
   {path:"planets", component: PlanetsComponent},
 ];
